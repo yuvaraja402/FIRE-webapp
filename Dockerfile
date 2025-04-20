72a08643ddf139f4eeffc4dbf4322bdee392b650
Yuva Raja <iampanneeryuvaraja@gmail.com>
2025-04-20 18:16:11 +0100

    Added few files

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,14 @@ FROM node:18-alpine
 # Set working directory
 WORKDIR /app
 
-# Copy package files and install dependencies
-#COPY client/package*.json ./
 # Copy the rest of the client app
 COPY client/ .
+
+# Initialize the Node.js project
+RUN npm init -y
 RUN npm install
-
-
+# Fixing Audit errors 
+RUN npm audit fix --force
 
 # Expose port (React dev server runs here)
 EXPOSE 3000
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        primary: '#1DA1F2',
-        secondary: '#14171A',
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1DA1F2',
+          secondary: '#14171A',
+        },
       },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
